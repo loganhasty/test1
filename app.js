cc38aff86f9089b67cf490f39f9099c8794a6639
Logan Hasty <loganhasty@gmail.com>
2017-07-19 21:16:18 -0400

    test2

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 
 $("#search").on("click", function(event) {
       event.preventDefault();
-      var queryURL = "https://api.transitandtrails.org/api/v1/trailheads.xml?key=4229776c13cfedab92d888c89c59178ef14793956a1f7fada15ff8507476e9cf";
+      var queryURL = "https://api.transitandtrails.org/api/v1/trailheads.xml";
 
       $.ajax({
           url: queryURL,
           method: "GET",
+
+          headers: ('Authorization: ', {
+            key: '4229776c13cfedab92d888c89c59178ef14793956a1f7fada15ff8507476e9cf'
+            
         })
         .done(function(response) {
           var results = response.data;
